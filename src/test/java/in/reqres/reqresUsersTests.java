c1cbabb5139a7fa8f549d4620f2571aed555b01f
Kate Belova <wallerina131@gmail.com>
2023-07-22 18:07:31 +0300

    initial commit

---
+++ src/test/java/in/reqres/reqresUsersTests.java
@@ -0,0 +1,102 @@
+package in.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class reqresUsersTests {
+
+    @Test
+    void getListOfUsersTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("total_pages", is(2));
+    }
+
+    @Test
+    void unsuccessfulLoginTest() {
+
+        String authData = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void updateAllInfoOfTheUserTest() {
+
+        String authData = "{ \"name\": \"morpheus\", \"job\": \"zion resident\", \"updatedAt\": \"2023-07-22T14:08:15.606Z\" }";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void updateSomeInfoOfTheUserTest() {
+
+        String authData = "{ \"name\": \"morpheus\", \"job\": \"zion resident\", \"updatedAt\": \"2023-07-22T14:12:08.492Z\" }";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUsersTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+    }
+
+}
